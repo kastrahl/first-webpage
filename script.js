850f23e0bda7e24ab86bd8c2f596ecf2edaffa5e
mrprince88 <51236601+mrprince88@users.noreply.github.com>
2019-10-06 18:13:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    function sayHello (event) {
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
+  }
+);
